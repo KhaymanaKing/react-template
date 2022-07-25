25ad1e2f819973c373565f8d8ea6f71ff975833f
Khayman King <khaymanak@gmail.com>
2022-07-25 15:00:35 -0700

    added routes to template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import Layout from './Page/layout';
+import List from './List';
+import About from './about';
+
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
+        <Route path="list" element={<List />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
