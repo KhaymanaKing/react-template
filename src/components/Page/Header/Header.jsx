7b45a39c42a1705c405e7e6fce8accf54f2e0a9e
Khayman King <khaymanak@gmail.com>
2022-08-04 12:42:55 -0700

    updated header component

--- src/components/Page/Header/Header.jsx
+++ src/components/Page/Header/Header.jsx
@@ -1,18 +1,26 @@
 import Navigation from './Navigation.jsx';
 import Menu from './Menu.jsx';
 import styles from './Header.css';
+import User from './User.jsx';
 
+const primary = [
+  { to:'/', label: 'Home' },
+  { to:'pokedex', label: 'Pokedex' },
+  { to:'fuzzy-funny', label: 'Fuzzy Bunny' },
+  { to:'about', label: 'About' },
+  // { to:'', label: '' },
+];
 export default function Header() {
   return (
     <header className={styles.Header}>
       <div className={styles.MenuContainer}>
-        <Menu/>
+        <Menu navigation={primary}/>
       </div>
       <h1> App </h1>
       <div className={styles.NavigationContainer}>
-        <Navigation/>
+        <Navigation navigation={primary}/>
       </div>
-      <div>User</div>
+      <User />
     </header>   
   );
 }
