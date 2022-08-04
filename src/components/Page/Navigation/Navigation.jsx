a4862c32e9918de84738644e33d8040773970fd5
Khayman King <khaymanak@gmail.com>
2022-08-04 12:49:32 -0700

    updated Navigation folder with all components

--- src/components/Page/Navigation/Navigation.jsx
+++ src/components/Page/Navigation/Navigation.jsx
@@ -2,18 +2,18 @@ import { Link } from 'react-router-dom';
 import styles from './Navigation.css';
 
 //navigation links for the popout menu. Links work menu does not as intended.
-export default function Navigation() {
+export default function Navigation({ navigation }) {
   return(
     <nav className={styles.Navigation}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="list">List</NavLink>
-      <NavLink to="about">About</NavLink>
-      <NavLink to="pokedex">Pokedex</NavLink>
+      {navigation.map(({ to, label }) => (
+        <NavLink key={to} to={to}>
+          {label}
+        </NavLink>
+      ))}
     </nav>
   );
 }
 
-// eslint-disable-next-line react/prop-types
 function NavLink({ children, ...rest }) {
   return (
     <Link className="label-text" {...rest}>
